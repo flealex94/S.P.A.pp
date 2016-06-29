9eda8ce09a889f05f7d6529f74b21acf2093be03
Alex Manescu <alexandrugmanescu@gmail.com>
2016-06-30 00:42:08 +0300

    Configurare
    o incercare de ecran de configurare

---
+++ src/Views/ConfigurationFrame.java
@@ -0,0 +1,12 @@
+package Views;
+
+import javax.swing.*;
+
+/**
+ * Created by flea on 30-Jun-16.
+ */
+public class ConfigurationFrame {
+    private JLabel titleLabel;
+    private JList list1;
+    private JList list2;
+}
